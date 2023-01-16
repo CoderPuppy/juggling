3f04e8964883d7357eaa19384a00c60afa20b699
CoderPuppy <coderpup@gmail.com>
2023-01-15 21:08:13 -0500

    Reorganize

---
+++ generators/SymmetricSync.hs
@@ -0,0 +1,49 @@
+{-# OPTIONS_GHC -Wno-tabs #-}
+{-# LANGUAGE MultiWayIf #-}
+
+module SymmetricSync where
+
+import Control.Monad
+import Data.Bits
+import Data.Foldable
+import Data.Tuple
+import Debug.Trace
+
+-- generating symmetric synchronous patterns
+--
+-- (4x,2x)(4,2x)* ≡ (4x,2x)(4,2x)(2x,4x)(2x,4)
+-- (6x,4x)* ≡ (6x,4x)(4x,6x) BAD
+
+effectiveHeight place height crossing = if
+	| not crossing -> 2 * height
+	| testBit place 0 -> 2 * height - 1
+	| otherwise -> 2 * height + 1
+landing n place height crossing = mod (place + effectiveHeight place height crossing) (4 * n)
+fill1 n place open = do
+	height <- [0..2 * n - 1]
+	crossing <- [False, True]
+	let landing1 = landing n place height crossing
+	let landing2 = landing n (complementBit (place + 2 * n) 0) height crossing
+	guard $ testBit open landing1
+	guard $ testBit open landing2
+	pure ((height, crossing), clearBit (clearBit open landing1) landing2)
+generate n = fmap fst $ foldrM
+	(\place (throws, open) -> do
+		(throw, open') <- fill1 n place open
+		pure (throw:throws, open'))
+	([], bit (4 * n) - 1 :: Integer) [0..2 * n - 1]
+addHeight minHeight maxHeight p h | h > maxHeight = []
+addHeight minHeight maxHeight p h | h < minHeight = addHeight minHeight maxHeight p (h + p)
+addHeight minHeight maxHeight p h = h:addHeight minHeight maxHeight p (h + p)
+pp [] = "*"
+pp ((l, lx):(r, rx):ss) = mconcat
+	[ "(", show (2 * l), if lx then "x" else ""
+	, ",", show (2 * r), if rx then "x" else ""
+	, ")", pp ss ]
+
+test = join $ traverse (traverse (fmap swap . traverse (addHeight minHeight maxHeight period) . swap)) $ generate period
+	where
+		period = 2 -- (_,_)(_,_)
+		minHeight = 1 -- 2
+		maxHeight = 6 -- c
+-- traverse_ putStrLn $ fmap pp $ filter (\ss -> all (\(h, _) -> h > 0 && h <= 4) ss && sum (fmap fst ss) == 2 * 5) $ fmap (fmap $ first \h -> if h == 0 then 4 else h) $ generate 2
